613aaf92f9a74e5173830962acdecb489b05ba93
Abdou kdj <abdoukadjoudj@gmail.com>
2025-11-21 11:55:46 +0100

    feat: hna ra7 tkon data ta3na this is a simple one f real world ra7 tkon mrbota b databse w t5dm dynamically

---
+++ lib/provider/TaskModel.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:week4_task/model/Task.dart';
+/*
+ @ todo model n7tajoh f les interfaces ta3na 
+
+ */
+
+class Taskmodel extends ChangeNotifier {
+  final List<Task> _todotasks = [
+    Task(
+      "Task 1",
+      false,
+      "Create Provider",
+      DateTime.now().add(Duration(days: 1)),
+    ),
+    Task(
+      "Task 1",
+      false,
+      "Create Provider",
+      DateTime.now().subtract(Duration(days: 1)),
+    ),
+    Task(
+      "Task 1",
+      false,
+      "Create Provider",
+      DateTime.now().add(Duration(days: 3)),
+    ),
+    Task(
+      "Task 4",
+      false,
+      "Create Provider",
+      DateTime.now().add(Duration(days: 4)),
+    ),
+  ];
+
+  List<Task> get todotasks => _todotasks;
+  void markDone(int index, bool _status) {
+    _todotasks[index].status = _status;
+    notifyListeners();
+  }
+}
