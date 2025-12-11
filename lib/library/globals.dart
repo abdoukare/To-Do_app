2de90bd7cc01ac787b8802793a1a760e78c4106f
Abdou kdj <abdoukadjoudj@gmail.com>
2025-12-11 19:52:29 +0100

    creating a list of most used colors

---
+++ lib/library/globals.dart
@@ -0,0 +1,29 @@
+ 
+ 
+ import 'package:flutter/material.dart';
+
+/// The Material Design primary color swatches, excluding grey.
+  const List<MaterialColor> primaries = <MaterialColor>[
+    Colors.red,
+    Colors.pink,
+    Colors.purple,
+    Colors.deepPurple,
+    Colors.indigo,
+    Colors.blue,
+    Colors.lightBlue,
+    Colors.cyan,
+    Colors.teal,
+    Colors.green,
+    Colors.lightGreen,
+    Colors.lime,
+    Colors.yellow,
+    Colors.amber,
+    Colors.orange,
+    Colors.deepOrange,
+    Colors.brown,
+    // The grey swatch is intentionally omitted because when picking a color
+    // randomly from this list to colorize an application, picking grey suddenly
+    // makes the app look disabled.
+    Colors.blueGrey,
+  ];
+  
